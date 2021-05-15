f2c7ca0fadf899d3415834dd87831338d8787eee
Nicholas Long <Nicholas.J.Long2018@gmail.com>
2021-05-15 06:06:39 -0700

    attempted to update routing properties

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,30 @@
-const express = require('express')
+//const express = require('express')
 const app = express()
+const fs = require('fs');
+const path = require('path');
+/*var router = express.Router();
 
-const PORT = 4000
+router.get('./index.js', function (req, res) {
+    res.send('Note taker');
+})
 
+router.get('/index.js', function (req, res) {
+    res.send('About this note taker');
+})
+
+module.exports = router;*/
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+var express = require('./index.js');
+app.use('/index', express);
 app.set('view engine', 'uuid')
 
 app.get(`${__dirname}/index.html`, (req, res) => {
     res.send('Hello World')
 })
 
-app.listen(4000)
+app.listen(PORT, function() {
+    console.log('App listening on PORT:' + PORT);
+});
